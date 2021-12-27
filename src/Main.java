bf3cdaf5197fcb7f4526f082e92ef23e1d88cf60
habebaRabie <habeba.rabiee@outlook.com>
2021-12-27 20:14:48 +0200

    add priority scheduling

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int size = Integer.parseInt(input.nextLine());
-        ArrayList<Process> processes = new ArrayList<>();
+        ArrayList<Process> processes1 = new ArrayList<>();
+        ArrayList<Process> processes2 = new ArrayList<>();
         while (size != 0) {
-            String name = input.nextLine();
-            int arrivalTime = Integer.parseInt(input.nextLine());
-            int burstTime = Integer.parseInt(input.nextLine());
-            int priorityNumber = Integer.parseInt(input.nextLine());
-            processes.add(new Process(name, Color.BLACK, arrivalTime, burstTime, priorityNumber, processes.size() - 1));
+            String name = input.next();
+            int arrivalTime = Integer.parseInt(input.next());
+            int burstTime = Integer.parseInt(input.next());
+            int priorityNumber = Integer.parseInt(input.next());
+            processes1.add(new Process(name, Color.BLACK, arrivalTime, burstTime, priorityNumber, processes1.size() - 1));
+            processes2.add(new Process(name, Color.BLACK, arrivalTime, burstTime, priorityNumber, processes2.size() - 1));
             size--;
         }
-        shortestJobFirst.start(processes, 11);
+
+        shortestJobFirst.start(processes1, 11);
+        System.out.println();
+        PriorityScheduling.start(processes2, 11);
     }
 }
---
+++ src/PriorityScheduling.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class PriorityScheduling {
+
+    public static void start(ArrayList<Process> processes, int age) {
+        processes.sort(new SJFComparator());
+        int time = processes.get(0).arrivalTime;
+        while (!processes.isEmpty()) {
+            int shortest = 0;
+            for (int j = 0; j < processes.size() && processes.get(j).arrivalTime <= time; j++) {
+                if (time - processes.get(j).arrivalTime >= age) {
+                    shortest = j;
+                    break;
+                } else if (processes.get(j).priorityNumber <= processes.get(shortest).priorityNumber) {
+                    shortest = j;
+                }
+            }
+            System.out.println(processes.get(shortest).Name);
+            time += processes.get(shortest).burstTime;
+            processes.remove(shortest);
+        }
+    }
+
+    public static class SJFComparator implements Comparator<Process> {
+        // arrive, burst, age
+        @Override
+        public int compare(Process o1, Process o2) {
+            if (o1.arrivalTime != o2.arrivalTime)
+                return o1.arrivalTime - o2.arrivalTime;
+            return o1.priorityNumber - o2.priorityNumber;
+        }
+    }
+}
